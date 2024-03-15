ad1fc9e70fd3da1b35d9c366b58219a9233247bf
chandrakalakakarla <123530194+chandrakalakakarla@users.noreply.github.com>
2024-03-16 00:52:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,10 @@ dotenv.config();
 const port = process.env.PORT || 6000
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static('../build'))
+    app.use('/', express.static('../frontend/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../build/index.html'))
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
     })
 }
 
